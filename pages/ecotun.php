218f0d140be781329521f9c7d670b0a8d34d1e69
wajihB <wajih.bouhlel@gmail.com>
2018-03-05 01:46:05 +0100

    Ajout Bootstrap + amelioration style
    ajout de bootstrap + amelioration du style de la page d'accueil

--- pages/ecotun.php
+++ pages/ecotun.php
@@ -4,26 +4,55 @@ if(isset($_SESSION['utilisateur'])){
     $utilisateur = $_SESSION['utilisateur'];
 }
 ?>
+
 <html>
+<body>
 
-<body background="../images/ecommerce.jpg">
-
-    <link rel="stylesheet" href="../Style.css" />
-
-    <h1>E-coTun</h1>
-
-
+    <link rel="stylesheet" href="../css/Style.css" />
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     
-    <p class="introduction">Livraison 3J ouvre // satisfait ou rembourse // qualite reconnu et norme CE</p>
+<!-- ajout du bandeau en haut de page -->
 
-    <br>
+    <div class="container-fluid bandeau"></div>
 
-    <div class="barre_recherche">
-        <form action="" class="recherche">
-            <input class="champ" type="text" value="Rechercher un produit, une marque, une reference..." />
-            <input class="bouton" type="submit" value="R" />
-        </form>
+    <h1>E-coTun.com</h1>
+    
+<!-- Creation barre de recherche -->
+
+    <div class="form-group_barre_recherche">
+    <div class="input-group input-group-md icon-addon addon-md">
+        <input type="text" placeholder="Texte" name="" id="schbox" class="form-control">
+        <i class="icon icon-search"></i>
+        <span class="input-group-btn">
+            <button type="submit" class="btn btn-inverse">Rechercher</button>
+        </span>
     </div>
+    </div>
+
+<!-- Creation barre de navigation -->
+
+    <div class="navbar navbar-default">
+
+  <div class="navbar-header">
+
+    <a class="navbar-brand" href="ecotun.php">ACCUEIL</a>
+
+  </div>
+
+  <ul class="nav navbar-nav">
+
+    <li class="active"><a href="smartphone.html">Smartphone</a></li>
+
+    <li><a href="informatique.html">Informatique</a></li>
+
+    <li><a href="console_gaming.html">Console & Gaming</a></li>
+
+    <li><a href="#">Contact</a></li>
+
+  </ul>
+
+</div>
+
 
     <div class="inscription">
         <a href="formulaire.html" target="_blank">Inscription</href>
@@ -38,12 +67,27 @@ if(isset($_SESSION['utilisateur'])){
         else
         {
         ?>
-            <form action="../scripts/connexion.php" method="post">  
-            utilisateur : <input type="text" name="nom_utilisateur" id="nom_utilisateur"> 
-            <br>
-            password : <input type="text" name="mdp" id="mdp">
-            <button type="submit">CONNEXION</buttontype></button>
-                </form>
+
+<!-- ajout de la zone de connection (login et mots de passe) -->
+
+
+<form action="../scripts/connexion.php" method="post">
+
+  <div class="form-group_login">
+    <label for="exampleInputEmail1">Nom d'utilisateur</label>
+    <input type="text" class="form-control" name="nom_utilisateur" id="nom_utilisateur" aria-describedby="emailHelp" placeholder="Entrer le nom d'utilisateur"> 
+  </div>
+
+  <div class="form-group_password">
+    <label for="exampleInputPassword1">Mots de passe</label>
+    <input type="password" class="form-control" name="mdp" id="mdp" placeholder="mots de passe">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Connection</button>
+</form>
+        
+
+        
         <?php 
         } 
         ?>
@@ -54,36 +98,5 @@ if(isset($_SESSION['utilisateur'])){
         <button type="submit">PANIER</button></form>
 
     </div>
-
-    <br>
-
-    <table class="position_titre_produit">
-        <tr>
-
-            <div class="smartphone">
-                <td>
-                    <ul>
-                        <a href="smartphone.html"> Smartphone</a>
-                    </ul>
-                </td>
-            </div>
-
-            <div class="informatique">
-                <td>
-                    <ul>
-                        <a href="informatique.html"> Informatique</a>
-                    </ul>
-                </td>
-            </div>
-
-            <div class="console_gaming">
-                <td>
-                    <ul>
-                        <a href="console_gaming.html"> Console & Gaming</a>
-                    </ul>
-                </td>
-            </div>
-        </tr>
-    </table>
 
 </body>
